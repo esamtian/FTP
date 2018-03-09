e1d98582562fbca557f9a7c180a92c5e5a2de174
esamtian <37093900+esamtian@users.noreply.github.com>
2018-03-09 11:37:29 +0800

    Update ftp_download.py

--- ftp_download.py
+++ ftp_download.py
@@ -2,14 +2,17 @@
 ####################################################################
 # python 查看FTP是否能连接成功
 ####################################################################
+import os
+from ftplib import FTP_TLS
 
 USERNAME = 'eweitia'
 PASSWORD = 'Tw201803'
-
 FTP_SERVER_HOST = '153.88.99.232'
 FTP_SERVER_PORT = 22
-import os
-from ftplib import FTP_TLS
+FILE_REMOTE = r'otpo.csv'
+FILE_LOCAL = r'C:\users\eweitia\desktop\otpo.csv'
+BUFSIZE = 1024  # 设置缓冲器大小
+
 def main():
     ftp = FTP_TLS()
     ftp.connect(host = FTP_SERVER_HOST)
@@ -18,14 +21,11 @@ def main():
     ftp.cwd('/ebipmft-concurintelligenceextract-vd')
     ftp.dir()
     print(ftp.getwelcome())
-
-    file_remote = r'otpo.csv'
-    file_local = r'C:\users\eweitia\desktop\otpo.csv'
-    bufsize = 1024  # 设置缓冲器大小
-    fp = open(file_local, 'wb')
-    ftp.retrbinary('RETR %s' % file_remote, fp.write, bufsize)
-
+    fp = open(FILE_LOCAL, 'wb')
+    ftp.retrbinary('RETR %s' % FILE_REMOTE, fp.write, BUFSIZE)
     ftp.quit()
     ftp.close()
+    
+    
 if __name__ == '__main__':
     main()
